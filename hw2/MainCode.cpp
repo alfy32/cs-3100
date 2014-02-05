8ab30d76daf44ef9a16037636699395594438d2f
Alan Christensen <alfy32@gmail.com>
2014-02-05 11:45:29 -0700

    Create MainCode.cpp

---
+++ hw2/MainCode.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "MyIostream.hpp"
+
+int main()
+{
+	usu::cout << "I love CS 3100 so much, let me tell you how...\n";
+	usu::cout << "I love it more than integers: " << 100 << "\n";
+	usu::cout << "I love it more than float point: " << 3.1415926 << "\n";
+
+	return 0;
+}
